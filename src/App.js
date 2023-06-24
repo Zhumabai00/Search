dbef0f4a7e047abf7039c15ec439407e455bfda8
user <user@MacBook-Pro.local>
2023-06-25 02:32:46 +0600

    V0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]) // create main hook for saving all datas
+  const getUsers = () => { /// function for getting the data of date
+    // axios.get('https://fakestoreapi.com/products')
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        setUsers(response.data)
+      })
+  }
+  useEffect(() => { /// activate when starting
+    getUsers()
+  }, [])
+
+  const [value, setValue] = useState('') /// saving new data after filtering
+
+  const filterUsers = users.filter(item => { /// the filtering
+    return item.name.toLowerCase().includes(value.toLowerCase())
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form">
+        <input
+          type="text"
+          placeholder='Search the users'
+          className='item__input'
+          onChange={(event) => setValue(event.target.value)} /// receive the contents from customers
+        />
+      </div>
+      <div className="users">
+        {
+          filterUsers.map((item) => {
+            return (
+              <>
+                <h1>{item.name}</h1>
+                {/* <h1>{item.username}</h1> */}
+              </>
+            )
+          })
+        }
+      </div>
+
     </div>
   );
 }
